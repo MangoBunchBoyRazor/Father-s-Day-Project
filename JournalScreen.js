e966bd46408756bad109c37da5aeeec0138950c6
MohamedRazeen102006 <mohd.rizwan@gmail.com>
2020-06-19 19:57:40 +0530

    Added extra features

--- JournalScreen.js
+++ JournalScreen.js
@@ -8,7 +8,7 @@ function searchKeysForEntry(){
     let textarea = elements[4];
     for(i = 0; i < journalKeys.length; i++){
         if(nameInput.value() == journalKeys[i]){
-            textarea.value(journalData.val()[journalKeys[i]].text);
+            textarea.contentDocument.body.innerHTML = journalData.val()[journalKeys[i]].text;
             return true;
         }
     }
@@ -17,10 +17,42 @@ function searchKeysForEntry(){
 function saveEntry(){
     let textarea = elements[4];
     let nameInput = elements[2];
-    if(textarea.value()){
+    if(textarea.contentDocument.body.innerHTML){
         database.ref(`Entries/${nameInput.value()}`).set({
             name: nameInput.value(),
-            text: textarea.value()
+            text: textarea.contentDocument.body.innerHTML
         });
     }
+}
+function execCmdBold(){
+    let rtfld = elements[4].contentDocument;
+    rtfld.execCommand('bold',false,null);
+}
+function execCmdItalics(){
+    let rtfld = elements[4].contentDocument;
+    rtfld.execCommand('italic',false,null);
+}
+function execCmdAlignCenter(){
+    let rtfld = elements[4].contentDocument;
+    rtfld.execCommand('justifyCenter',false,null);
+}
+function execCmdAlignJustify(){
+    let rtfld = elements[4].contentDocument;
+    rtfld.execCommand('justifyFull',false,null);
+}
+function execCmdAlignLeft(){
+    let rtfld = elements[4].contentDocument;
+    rtfld.execCommand('justifyLeft',false,null);
+}
+function execCmdAlignRight(){
+    let rtfld = elements[4].contentDocument;
+    rtfld.execCommand('justifyRight',false,null);
+}
+function execCmdUndo(){
+    let rtfld = elements[4].contentDocument;
+    rtfld.execCommand('undo',false,null);
+}
+function execCmdRedo(){
+    let rtfld = elements[4].contentDocument;
+    rtfld.execCommand('redo',false,null);
 }
